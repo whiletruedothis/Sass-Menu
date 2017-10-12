ffe4dc668ca04142999403f54523edf48a9afb7a
Rodrigo Torrico <rodrigotorrico@192.168.1.6>
2017-10-12 13:21:22 -0400

    fixed menu

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 $( document ).ready(
-    $(document).on('click',function(e){
+   $(document).on('click',function(e){
         if(($(e.target).prop("class") == "logo")){
-            $('.nav').addClass('nav--show');
+            $("ul.nav").toggleClass( "nav--show" )
         }
         else{
             $('.nav').removeClass('nav--show');
         }
     }),
+
+   /* $("button").click(function(){
+        $("ul.nav").toggleClass( "nav--show" )
+    })*/
 )
+
+
 
